cea3798fd548dbaebb5996d554deb449716f56e9
FelipeCampbell <37164868+FelipeCampbell@users.noreply.github.com>
2022-06-03 14:55:30 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Svelte Playground
+
+This is a `svelte` playground to start a new project. It comes with `tailwindcss`, `stylelint` & `eslint`.
+
+**Assumes `prettier` is already configured.**
+
+---
+
+## Usage
+
+`npm install` to install the required node modules.
+
+`npm run dev` to run dev server with live preview.
+
+`npm run build` to build for production.
+
+---
+
+## **Check out [Svelte](https://svelte.dev/)** for more info.
+
+### Feel free to use this as a [template](https://github.com/kr40/svelte-tailwind-playground/generate).
---
+++ src/App.svelte
@@ -0,0 +1,60 @@
+<script>
+	import Counter from './lib/Counter.svelte';
+	import Tailwindcss from './Tailwindcss.svelte';
+</script>
+
+<Tailwindcss />
+
+<template>
+	<main>
+		<h1>Hello world!</h1>
+
+		<Counter />
+
+		<p>This is a Svelte Template which uses TailwindCSS & comes with Stylelint & Stylelint pre-configured.</p>
+
+		<p>
+			<strong class="text-xl text-red-600">Assumes Prettier is already configured.</strong>
+		</p>
+	</main>
+</template>
+
+<style style lang="postcss">
+	:root {
+		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans',
+			'Helvetica Neue', sans-serif;
+	}
+
+	main {
+		@apply text-center;
+		@apply p-4;
+		@apply mx-auto;
+	}
+
+	h1 {
+		@apply text-red-600;
+		@apply uppercase;
+		@apply text-6xl;
+		@apply font-thin;
+		@apply leading-tight;
+		@apply my-8 mx-auto;
+		@apply max-w-xs;
+	}
+
+	p {
+		@apply max-w-xs;
+
+		margin: 1rem auto;
+		@apply leading-snug;
+	}
+
+	@screen sm {
+		h1 {
+			@apply max-w-none;
+		}
+
+		p {
+			@apply max-w-none;
+		}
+	}
+</style>
